e22941394867b5002fe1989aecd9a472deab6422
pusteugene <pustoviteugene@gmail.com>
2025-06-18 14:06:32 +0300

    Update image paths in multiple HTML files to reflect new asset locations, ensuring consistency and improving asset management across the project.

--- xmlrpc.php
+++ xmlrpc.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://cryptofundtrader.com/</homePageLink>
+		<homePageLink></homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://cryptofundtrader.com/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://cryptofundtrader.com/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://cryptofundtrader.com/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://cryptofundtrader.com/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://cryptofundtrader.com/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="wp-json/" />
 			</apis>
 	</service>
 </rsd>
